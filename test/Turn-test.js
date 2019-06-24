5f53e74cc6369a5082ed94168faace8f2e891ddd
Alek <alekaker@outlook.com>
2019-06-24 16:47:30 -0600

    adding turn class

---
+++ test/Turn-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', function() {
+
+  it('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn)
+  });
+
+  it('should have a correct guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.guess).to.deep.equal(card.correctAnswer)
+  });
+
+});
