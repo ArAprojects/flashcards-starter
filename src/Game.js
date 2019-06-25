ebd50504b220aea311a0d37ba5acabe5466e59c0
Alek <alekaker@outlook.com>
2019-06-25 11:01:19 -0600

    add simple game class test

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,25 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round');
+const Card = require('../src/Card');
 
+const cardsy = prototypeQuestions.map(ob => ob = new Card(ob.id, ob.question, ob.))
+console.log(cardsy)
 class Game {
-  constructor() {}
+  constructor() {
+
+
+  }
+
+
+
+
+  start() {
+
+    this.printMessage()
+    this.printQuestion()
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
